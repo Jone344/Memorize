e2a0527f8cd2aff230b3d6a89d42ad71ab8179bd
mando <76523408+spenser-jony@users.noreply.github.com>
2024-04-11 13:02:09 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by user on 11.04.24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
