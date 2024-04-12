c3847fd2358e3b4f05ea948dda3458c984390246
mando <76523408+spenser-jony@users.noreply.github.com>
2024-04-12 17:30:52 +0200

    we

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,14 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis = ["😄", "😻", "👹", "🤠"]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            ForEach(emojis.indices, id: \.self) {index in
+                CardView(content: emojis[index])
+            }
         }
-        .padding()
+        .foregroundColor(.orange)
+         .padding()
+    }
+}
+
+struct CardView: View {
+    let content: String
+    @State var isFaceUp = true
+    
+    var body: some View {
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
+            if isFaceUp {
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text(content).font(.largeTitle)
+            } else {
+                base.fill()
+            }
+                
+        }
+        
+        .onTapGesture {
+            isFaceUp.toggle()
+            print("qwe")
+        }
     }
 }
 
