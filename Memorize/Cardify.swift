aaa61d8751f8ac45c9dc3a18b946841402668210
mando <76523408+spenser-jony@users.noreply.github.com>
2024-04-30 18:42:56 +0200

    L7

---
+++ Memorize/Cardify.swift
@@ -0,0 +1,8 @@
+//
+//  Cardify.swift
+//  Memorize
+//
+//  Created by user on 30.04.24.
+//
+
+import Foundation
---
+++ Memorize/Pie.swift
@@ -0,0 +1,8 @@
+//
+//  Pie.swift
+//  Memorize
+//
+//  Created by user on 30.04.24.
+//
+
+import Foundation
